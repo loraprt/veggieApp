795821b43c3bf09bc5c71f8f28b923866b083f8f
Lora <lora.poupart@comarch.com>
2023-11-03 11:08:20 +0100

    test

--- app/src/main/java/com/example/veggieapp/MainActivity.java
+++ app/src/main/java/com/example/veggieapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.veggieapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//je fais juste un test
 public class MainActivity extends AppCompatActivity {
 
     @Override
